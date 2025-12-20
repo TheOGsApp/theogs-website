13848e3e861fa34cd4c0300e276b1238279ac6e5
empro-narendervaddepelly <narender.vaddepelly@empro.de>
2025-12-20 14:57:32 +0100

    remove token

--- src/api/index.ts
+++ src/api/index.ts
@@ -16,8 +16,6 @@ export const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = Cookies.get(storageKeys.accessToken);
 
-  console.log('API Request Token:', token);
-
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
   }
